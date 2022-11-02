ef4269c418a6b5778024297c2d114fb7701f7c10
V2RA <61801118+V2RA@users.noreply.github.com>
2022-11-02 13:11:14 +0000

    Update NumberGuess.py

--- NumberGuess.py
+++ NumberGuess.py
@@ -1,4 +1,5 @@
 import random
+from typing import Counter
 
 try:
     import lightrun
@@ -13,9 +14,44 @@ trycount = 0
 mode = int(input())
 
 def hardMode():
-    num = random.randint(10000,99999)
-    print("Guess the number")
-    guess = int(input(""))
+	num = random.randint(10000,99999)
+	urmom = (random.randint(1,9), random.randint(1,9), random.randint(1,9), random.randint(1,9))
+	print("Guess the number")
+	print(num)
+	done = False
+	wrongCount = 0
+	while done != True:
+		
+		guess = int(input("Guess:   "))
+		guesslist = list(str(guess))
+		urmom2 = list(str(num))
+		numCorrect = 0
+		if guess != num:
+			wrongCount = wrongCount + 1
+			print("You got", numCorrect, "correct")
+		else:
+			print("You got it right!")
+			print("You failed", wrongCount, "times")
+			break
+		while len(str(guess)) != 5:
+			print("Invalid count of numbers")
+			easyMode()
+			break
+		else:
+#			print(guesslist, "guesslist")
+#			print(urmom2, "urmom2")
+			rml = 0
+			for c in range(0,5):
+	
+				if guesslist[rml] in urmom2:
+					if guesslist[rml] in urmom2[rml]:
+					#	print("number", guesslist[rml], "is in correct position")
+						numCorrect = numCorrect + 1
+					else:
+					#	print("number", guesslist[rml], "is in the wrong position")
+						rml = rml + 1
+				else:
+					rml = rml + 1
 
 
 def easyMode():
@@ -23,36 +59,36 @@ def easyMode():
 	urmom = (random.randint(1,9), random.randint(1,9), random.randint(1,9), random.randint(1,9))
 	print("Guess the number")
 	print(num)
-	guess = int(input())
-	guesslist = list(str(guess))
-	urmom2 = list(str(num))
-	while len(str(guess)) != 4:
-		print("Invalid count of numbers")
-		easyMode()
-		break
-	else:
-		print(guesslist, "guesslist")
-		print(urmom2, "urmom2")
-		print(guesslist[0])
-		rml = 0
-		for c in range(0,4):
-			if guess == num:
-				print("You got it!")
-				break
-
-			if guesslist[rml] in urmom2:
-				if guesslist[rml] in urmom2[rml]:
-					print("number", guesslist[rml], "is in correct position")
+	done = False
+	wrongCount = 0
+	while done != True:
+		
+		guess = int(input("Guess:   "))
+		guesslist = list(str(guess))
+		urmom2 = list(str(num))
+		if guess != num:
+			wrongCount = wrongCount + 1
+		else:
+			print("You got it right!")
+			print("You failed", wrongCount, "times")
+			break
+		while len(str(guess)) != 4 and guesslist[0] != "0":
+			print(guesslist[0])
+			print("Invalid count of numbers")
+			easyMode()
+			break
+		else:
+			rml = 0
+			for c in range(0,4):
+				if guesslist[rml] in urmom2:
+					if guesslist[rml] in urmom2[rml]:
+						print("number", guesslist[rml], "is in correct position")
+					else:
+						print("number", guesslist[rml], "is in the wrong position")
+					rml = rml + 1
 				else:
-				##	print("number", guesslist[rml], "is in position", (rml + 1))
-					print("number", guesslist[rml], "is in the wrong position")
-				rml = rml + 1
-				
-			else:
-				print("no")
-				rml = rml + 1
-            
-            
+					rml = rml + 1
+
 
 
 if mode == 1:
@@ -60,3 +96,6 @@ if mode == 1:
 
 if mode == 2:
     easyMode()
+
+
+## For http://www.ocr.org.uk/Images/202838-20-code-challenges.pdf
