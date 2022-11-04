5d496cbc9073cdaaeeba3f74d59fbc1fd357c7ea
V2RA <61801118+V2RA@users.noreply.github.com>
2022-11-04 12:53:00 +0000

    Update

--- NumberGuess.py
+++ NumberGuess.py
@@ -1,31 +1,44 @@
 import random
 from typing import Counter
 
-try:
-    import lightrun
-    lightrun.enable(company_key='c0d7ceb1-d646-41f4-956b-60a20078f09b')
-except ImportError as e:
-    print("Error importing Lightrun: ", e)
-
 print("Modes:")
-print("1 - Hard")
-print("2 - Easy")
+print("1 - Hard (5 digits)")
+print("2 - Easy (4 digits)")
+print("3 - Description for both")
 trycount = 0
 mode = int(input())
+
+if mode == 3:
+	print("Hard mode is 5 digits, and only shows how many you got right; not which ones you got right and which position")
+	print("Easy mode is 4 digits, and shows which number you got right")
+	mode = int(input("Choose:   "))
 
 def hardMode():
 	num = random.randint(10000,99999)
 	urmom = (random.randint(1,9), random.randint(1,9), random.randint(1,9), random.randint(1,9))
 	print("Guess the number")
-	print(num)
 	done = False
 	wrongCount = 0
 	while done != True:
-		
 		guess = int(input("Guess:   "))
 		guesslist = list(str(guess))
 		urmom2 = list(str(num))
 		numCorrect = 0
+		while len(str(guess)) != 5:
+			print("Invalid count of numbers")
+			hardMode()
+			break
+		else:
+			rml = 0
+			for c in range(0,5):
+				if guesslist[rml] in urmom2:
+					if guesslist[rml] in urmom2[rml]:
+						numCorrect = numCorrect + 1
+						rml = rml + 1
+					else:
+						rml = rml + 1
+				else:
+					rml = rml + 1
 		if guess != num:
 			wrongCount = wrongCount + 1
 			print("You got", numCorrect, "correct")
@@ -33,36 +46,16 @@ def hardMode():
 			print("You got it right!")
 			print("You failed", wrongCount, "times")
 			break
-		while len(str(guess)) != 5:
-			print("Invalid count of numbers")
-			easyMode()
-			break
-		else:
-#			print(guesslist, "guesslist")
-#			print(urmom2, "urmom2")
-			rml = 0
-			for c in range(0,5):
-	
-				if guesslist[rml] in urmom2:
-					if guesslist[rml] in urmom2[rml]:
-					#	print("number", guesslist[rml], "is in correct position")
-						numCorrect = numCorrect + 1
-					else:
-					#	print("number", guesslist[rml], "is in the wrong position")
-						rml = rml + 1
-				else:
-					rml = rml + 1
+
 
 
 def easyMode():
 	num = random.randint(1000,9999)
 	urmom = (random.randint(1,9), random.randint(1,9), random.randint(1,9), random.randint(1,9))
 	print("Guess the number")
-	print(num)
 	done = False
 	wrongCount = 0
 	while done != True:
-		
 		guess = int(input("Guess:   "))
 		guesslist = list(str(guess))
 		urmom2 = list(str(num))
@@ -73,7 +66,6 @@ def easyMode():
 			print("You failed", wrongCount, "times")
 			break
 		while len(str(guess)) != 4 and guesslist[0] != "0":
-			print(guesslist[0])
 			print("Invalid count of numbers")
 			easyMode()
 			break
@@ -90,12 +82,25 @@ def easyMode():
 					rml = rml + 1
 
 
-
 if mode == 1:
     hardMode()
 
 if mode == 2:
     easyMode()
+
+print("Would you like to go again?")
+goA = input()
+goAgain = goA.lower()
+if goAgain == "yes":
+	print("Which one?")
+	print("Modes:")
+	print("1 - Hard (5 digits)")
+	print("2 - Easy (4 digits)")
+	nall = int(input())
+	if nall == 1:
+		hardMode()
+	if nall == 2:
+		easyMode()
 
 
 ## For http://www.ocr.org.uk/Images/202838-20-code-challenges.pdf
